993b03af6755c5ec1add9e3c8080f72fe0561c8b
Ernst-Christoph Schrewe <132669361+eschrewe@users.noreply.github.com>
2026-07-22 11:35:53 +0200

    Update launcher/con-x-wallet-sql-vault/build.gradle.kts
    Co-authored-by: Malte Hellmeier <malte.hellmeier@isst.fraunhofer.de>

--- launcher/con-x-wallet-sql-vault/build.gradle.kts
+++ launcher/con-x-wallet-sql-vault/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026. Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e.V. (represented by Fraunhofer ISST)
+ * Copyright (c) 2026 Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e.V. (represented by Fraunhofer ISST)
  *
  * This program and the accompanying materials are made available under the
  * terms of the Apache License, Version 2.0 which is available at
